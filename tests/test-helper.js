7cc22446f57b03287be9b45b9fe444a910f1faf7
Gaurav Munjal <Gaurav0@aol.com>
2020-12-13 12:18:04 -0500

    update test blueprints

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'ember-test-assignment/app';
 import config from 'ember-test-assignment/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
